fe9a7160b26ae8e4e3caa514a674253b84cdcb71
bloobsky <email@example.com>
2022-11-02 08:08:44 +0000

    Custom Panel enabled

--- WindowCreator.java
+++ WindowCreator.java
@@ -8,5 +8,12 @@ public class WindowCreator {
         customWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         customWindow.setTitle("Shapes Manager - play with Shapes");
         customWindow.setVisible(true);
+
+        ShapesManager shapes_manager = new ShapesManager();
+        shapes_manager.enable_display_name(true);
+        shapes_manager.enable_display_bb(true);
+
+        shapes_manager.add_shape(new Circle( Color.RED, true, 100, 200, 50));
+
     }
 }
