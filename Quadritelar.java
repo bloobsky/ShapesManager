6d71f884558bd1528939ea4bbad018f027b6dbe4
bloobsky <bloobsky@gmail.com>
2022-11-05 17:25:52 +0100

    Added Rotation for Quad

--- Quadritelar.java
+++ Quadritelar.java
@@ -58,4 +58,32 @@ public class Quadritelar extends Shape {
         return new BoundingBox(new Point(x1,y1),new Point(x2,y2));
 
     }
+
+    public void rotate_unit() {
+        for (Point p: points) {
+            rotate_it(p, new Point(get_x_centre(), get_y_centre()));
+        }
+    }
+
+    private void rotate_it(Point p, Point centre_point) {
+        int rotation_degree = 90; 
+        double mCos = Math.cos(Math.toRadians(rotation_degree));
+        double mSin = Math.sin(Math.toRadians(rotation_degree));
+
+        int point_x = p.get_x();
+        int point_y = p.get_y();
+
+        int centre_x = centre_point.get_x();
+        int centre_y = centre_point.get_y();
+
+
+        double rlx = (((point_x - centre_x) * mCos) - ((point_y - centre_y)*mSin));
+        double rly = (((point_x - centre_x) * mSin) + ((point_y) - centre_y)*mCos));
+
+        //Math.round to eliminate 'cast to int' rounding error
+        p.set_x( (int) Math.round(rlx + centre_point.get_x()));
+        p.set_y( (int) Math.round(rly + centre_point.get_y()));
+
+    }
 }
+
